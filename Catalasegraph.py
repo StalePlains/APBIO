9c49e47aee08d16e72d620de29d5cd850ee9e461
Maxwell Greene <77181434+StalePlains@users.noreply.github.com>
2023-11-06 18:45:35 -0500

    Adding comments for explanation

--- Catalasegraph.py
+++ Catalasegraph.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# This is me adding all the necessary data and calculating the margins for percent error to be added to the graph
 concentrations = np.array(['0', '0.375', '0.75', '1.5', '3'])
 
 means = np.array([0, 0.0663, 0.1556, 0.2303, 0.4135])
 standard_errors = np.array([0, 0.0054, 0.0275, 0.0684, 0.1947])
 standard_errors = standard_errors * 2
 
+# This is me labelling and creating the bars of the graph
 plt.xlabel("Concentrations of Hydrogen Peroxide (%)")
 plt.ylabel("Mean Rate of Reaction (mol/s)")
 plt.title("Concentrations of Hydrogen Peroxide (%) vs Mean Rate of Reaction (mol/s)")
