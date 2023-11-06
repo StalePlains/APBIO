2b29f19201537ec2ab328c2334d77a5f637dbacf
Maxwell Greene <77181434+StalePlains@users.noreply.github.com>
2023-11-05 19:03:50 -0500

    Update Catalasegraph.py
    got rid of unnecessary print statement

--- Catalasegraph.py
+++ Catalasegraph.py
@@ -7,8 +7,6 @@ means = np.array([0, 0.0663, 0.1556, 0.2303, 0.4135])
 standard_errors = np.array([0, 0.0054, 0.0275, 0.0684, 0.1947])
 standard_errors = standard_errors * 2
 
-print(standard_errors)
-
 plt.xlabel("Mean Concentrations of Hydrogen Peroxide (%)")
 plt.ylabel("Rate of Reaction (mol/s)")
 plt.title("Mean Concentrations of Hydrogen Peroxide (%) vs Rate of Reaction (mol/s)")
